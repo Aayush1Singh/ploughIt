ee3f292f2dab30d12ce271fedafa5f8c91a0a983
aayush <aayush>
2025-01-21 01:27:45 +0530

    improving backend

--- backend/index.js
+++ backend/index.js
@@ -2,29 +2,26 @@ import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import mysql from "mysql";
 const salt = 10;
 const jwtSecret = "tusharisgay";
-const app = express();
-
-app.use(express.json());
 const corsOption = {
   origin: "*",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 };
-app.use(cors(corsOption));
-import mysql from "mysql";
-import { skipPartiallyEmittedExpressions } from "typescript";
 
-// configurations for creating mysql connection
+const app = express();
+app.use(express.json());
+app.use(cors(corsOption));
+
 const con = mysql.createConnection({
-  host: "localhost", // host for connection
-  port: 3306, // default port for mysql is 3306
-  database: "ploughIt", // database from which we want to connect our node application
-  user: "root", // username of the mysql connection
-  password: "aayush", // password of the mysql connection
+  host: "localhost",
+  port: 3306,
+  database: "ploughIt",
+  user: "root",
+  password: "aayush",
 });
-// executing connection
 con.connect(function (err) {
   if (err) {
     console.log("error occurred while connecting");
@@ -34,20 +31,69 @@ con.connect(function (err) {
 });
 
 app.get("/demand/insert", (req, res) => {
-  const data = req.headers;
-  console.log(data);
+  const {
+    crop,
+    variety,
+    quantity,
+    price,
+    duration,
+    preference,
+    description,
+    id,
+  } = req.headers;
+  if (
+    !crop ||
+    !variety ||
+    !quantity ||
+    !price ||
+    !duration ||
+    !preference ||
+    !id
+  ) {
+    res.status(400).send({
+      status: "failed",
+      message: "Failed to create Demand.Validation Error",
+      errors: {
+        crop: !crop ? "Crop cannot be empty." : undefined,
+        variety: !variety ? "Variety cannot be empty." : undefined,
+        quantity:
+          quantity <= 0 ? "Quantity must be greater than zero." : undefined,
+        price:
+          price <= 0 ? "Price per unit must be greater than zero." : undefined,
+        duration:
+          duration <= 0 ? "Duration must be greater than zero." : undefined,
+      },
+    });
+    return;
+  }
+
   const result = con.query(
-    `insert into demand(crop,variety,quantity,price,duration,preference,description,contractorID) values("${data.crop}","${data.variety}",${data.quantity},${data.price},${data.duration},"${data.preference}","${data.description}","${data.id}");`
+    `insert into demand(crop,variety,quantity,price,duration,preference,description,contractorID) values("${crop}","${variety}",${quantity},${price},${duration},"${preference}","${description}","${id}");`,
+    (err, result) => {
+      if (err) {
+        req
+          .status(505)
+          .send({ status: "failed", message: "Failed to insert Demand" });
+      } else
+        res.status(201).send({
+          status: "success",
+          message: "Demand successfully inserted",
+        });
+      return;
+    }
   );
-  console.log("added");
-  res.send("inserted");
 });
 
 app.get("/demand/search", (req, res) => {
   const { crop, variety, price, quantity, preference, duration, PAGE_SIZE } =
     JSON.parse(req.headers.data);
-  console.log(JSON.parse(req.headers.data));
-  console.log("helooooooooooooooooooooooooooooooooooooooooooooooooo");
+  if (!duration) {
+    res.status(400).send({
+      status: "failed",
+      message: "Invalid duration sent",
+    });
+    return;
+  }
   con.query(
     `select * from demand where quantity between ${quantity[0]} and ${
       quantity[1]
@@ -58,12 +104,18 @@ app.get("/demand/search", (req, res) => {
     } ${
       variety.length > 0 ? `and variety in ("${variety.join(`","`)}")` : ""
     } order by auto_id limit ${PAGE_SIZE + 1}`,
-    (err, result, fields) => {
-      console.log(result);
+    (err, result) => {
+      if (err) {
+        res
+          .status(505)
+          .send({ status: "failed", message: "Data could not be fetched" });
+        return;
+      }
       let hasMore = true;
       if (result.length != PAGE_SIZE + 1) hasMore = false;
-      if (result.length == PAGE_SIZE + 1) result.pop();
-      res.send({
+      else result.pop();
+
+      res.status(200).send({
         result,
         cursors: {
           next: result.length === 0 ? -1 : result[result.length - 1].auto_id,
@@ -71,19 +123,37 @@ app.get("/demand/search", (req, res) => {
           hasMore,
         },
         auto_id: result.length === 0 ? -1 : result[result.length - 1].auto_id,
+        status: "success",
+        message: "data successfully fetched",
       });
+      return;
     }
   );
-  // console.log(result);
-  // res.send("x");
 });
 app.get("/demand/search/id", (req, res) => {
-  con.query(
-    `select * from demand where auto_id=${JSON.parse(req.headers.data).id}`,
-    (err, result) => {
-      res.send(result);
-    }
-  );
+  const { id } = JSON.parse(req.headers.data);
+  if (!id) {
+    res.status(400).send({
+      status: "failed",
+      message: "Invalid DemandID searched",
+    });
+    return;
+  }
+  con.query(`select * from demand where auto_id=${id}`, (err, result) => {
+    if (err) {
+      res.status(505).send({
+        status: "failed",
+        message: "Data could not be fetched",
+      });
+      return;
+    } else
+      res.status(200).send({
+        result,
+        status: "success",
+        message: "data successfully fetched",
+      });
+    return;
+  });
 });
 app.get("/demand/search/prev", (req, res) => {
   const {
@@ -96,6 +166,13 @@ app.get("/demand/search/prev", (req, res) => {
     PAGE_SIZE,
     cursors,
   } = JSON.parse(req.headers.data);
+  if (!duration || !cursors?.prev) {
+    res.status(400).send({
+      status: "failed",
+      message: "Could not find duration or cursors",
+    });
+    return;
+  }
   con.query(
     `select * from demand where quantity between ${quantity[0]} and ${
       quantity[1]
@@ -105,15 +182,17 @@ app.get("/demand/search/prev", (req, res) => {
       crop.length > 0 ? `and crop in ("${crop.join(`","`)}")` : ""
     } ${
       variety.length > 0 ? `and variety in ("${variety.join(`","`)}")` : ""
-    } and auto_id<${cursors.prev} order by auto_id desc limit ${
-      PAGE_SIZE + 1
-    };`,
-    (err, result, fields) => {
-      console.log(result);
+    } and auto_id<${cursors.prev} order by auto_id desc limit ${PAGE_SIZE};`,
+    (err, result) => {
+      if (err) {
+        res.status(505).send({
+          status: "failed",
+          message: "Data could not be fetched",
+        });
+        return;
+      }
       result.reverse();
       let hasMore = true;
-      if (result.length != PAGE_SIZE + 1) hasMore = false;
-      if (result.length == PAGE_SIZE + 1) result.pop();
       res.send({
         result,
         cursors: {
@@ -122,7 +201,10 @@ app.get("/demand/search/prev", (req, res) => {
           hasMore: true,
         },
         auto_id: result.length === 0 ? -1 : result[0].auto_id,
+        status: "success",
+        message: "data successfully fetched",
       });
+      return;
     }
   );
 });
@@ -138,18 +220,18 @@ app.get("/demand/search/next", (req, res) => {
     PAGE_SIZE,
     cursors,
   } = JSON.parse(req.headers.data);
-  console.log("hello");
-  console.log(
-    `select * from demand where quantity between ${quantity[0]} and ${
-      quantity[1]
-    } and duration between ${duration[0]} and ${duration[1]} ${
-      preference === "" ? "" : `and preference like "${preference}"`
-    } and price>=${price} ${
-      crop.length > 0 ? `and crop in ("${crop.join(`","`)}")` : ""
-    } ${
-      variety.length > 0 ? `and variety in ("${variety.join(`","`)}")` : ""
-    } and auto_id>${cursors.next} order by auto_id limit ${PAGE_SIZE}`
-  );
+  if (!quantity || !duration || !cursors?.next) {
+    res.status(400).send({
+      status: "failed",
+      message: "Data received incorrect",
+      errors: {
+        quantity: !quantity ? "Quantity is not defined" : undefined,
+        duration: !duration ? "Duration is not defined" : undefined,
+        cursors: !cursors?.next ? "cursor is not defined" : undefined,
+      },
+    });
+    return;
+  }
   con.query(
     `select * from demand where quantity between ${quantity[0]} and ${
       quantity[1]
@@ -161,11 +243,16 @@ app.get("/demand/search/next", (req, res) => {
       variety.length > 0 ? `and variety in ("${variety.join(`","`)}")` : ""
     } and auto_id>${cursors.next} order by auto_id limit ${PAGE_SIZE + 1};`,
     (err, result) => {
-      if (err) console.log(err);
-      console.log(result, "lll");
+      if (err) {
+        res.status(505).send({
+          status: "failed",
+          message: "Data could not be fetched",
+        });
+        return;
+      }
       let hasMore = true;
       if (result.length != PAGE_SIZE + 1) hasMore = false;
-      if (result.length == PAGE_SIZE + 1) result.pop();
+      else result.pop();
 
       res.send({
         result,
@@ -175,186 +262,232 @@ app.get("/demand/search/next", (req, res) => {
           hasMore,
         },
         auto_id: result.length === 0 ? -1 : result[0].auto_id,
+        status: "success",
+        message: "data successfully fetched",
       });
     }
   );
 });
-/*{
-  "success": true,
-  "message": "User details retrieved successfully.",
-  "data": {
-    "id": 1,
-    "name": "Aayush Singh",
-    "email": "aayush3708@gmail.com",
-    "phone": "8076442909"
-  },
-  "errors": [],
-  "metadata": {
-    "timestamp": "2025-01-06T10:10:00Z"
-  }
-} */
+
 app.post("/signup/contractor", (req, res) => {
-  console.log(req.body.data);
-  const { result } = con.query(
-    `select * from contractor where email="${req.body.email}";`,
+  const { email, password, contractor_name } = req.body;
+  con.query(
+    `select * from contractor where email="${email}";`,
     (err, result) => {
+      if (err) {
+        res.status(505).send({
+          status: "failed",
+          message: "Data could not be verified/inserted",
+        });
+        return;
+      }
       if (result?.length > 0) {
-        console.log("hello");
-        res.status(400).send({ error: "email not registered" });
+        res
+          .status(400)
+          .send({ status: "failed", message: "email registered already" });
+        return;
       }
 
-      bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
-        if (err) throw new Error(err.message);
+      bcrypt.hash(password.toString(), salt, (err, hash) => {
+        if (err) {
+          res
+            .status(505)
+            .send({ status: "failed", message: "User could not be created" });
+          return;
+        }
 
         con.query(
-          `insert into contractor(contractor_name,email,password) values("${req.body.contractor_name}","${req.body.email}","${hash}")`
-        );
+          `insert into contractor(contractor_name,email,password) values("${contractor_name}","${email}","${hash}")`,
+          (err, result) => {
+            if (err) {
+              res.status(505).send({
+                status: "failed",
+                message: "User could not be created",
+              });
+              return;
+            }
 
-        res.send("ok");
+            res.status(200).send({
+              status: "success",
+              message: "User created",
+            });
+            return;
+          }
+        );
       });
     }
   );
-  console.log(`select * from contractor where email="${req.body.email}";`);
-  // console.log(result);
 });
 app.post("/signup/farmer", (req, res) => {
-  console.log(req.body.data);
+  const { email, password } = req.body;
   const { result } = con.query(
-    `select * from farmer where email="${req.body.email}";`,
+    `select * from farmer where email="${email}";`,
     (err, result) => {
       if (result?.length > 0) {
-        console.log("hello");
-        res.status(400).send({ error: "email not registered" });
+        res
+          .status(400)
+          .send({ status: "failed", error: "email not registered" });
+        return;
       }
 
-      bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
-        if (err) throw new Error(err.message);
-
+      bcrypt.hash(password.toString(), salt, (err, hash) => {
+        if (err) {
+          res.status(505).send({
+            status: "failed",
+            message: "User could not be created",
+          });
+          return;
+        }
         con.query(
-          `insert into farmer(email,password) values("${req.body.email}","${hash}")`
-        );
+          `insert into farmer(email,password) values("${email}","${hash}")`,
+          (err, result) => {
+            if (err) {
+              res.status(505).send({
+                status: "failed",
+                message: "User could not be created",
+              });
+              return;
+            }
 
-        res.send("ok");
+            res.status(200).send({
+              status: "success",
+              message: "User created",
+            });
+            return;
+          }
+        );
       });
     }
   );
-  console.log(`select * from farmer where email="${req.body.email}";`);
-  // console.log(result);
 });
 app.get("/signin", (req, res) => {
   // console.log(req.headers);
-  const a = async function () {
-    console.log(
-      `select * from ${req.headers.password} where email="${req.headers.email}"`
-    );
-    const { result } = await con.query(
-      `select * from ${req.headers.role} where email="${req.headers.email}"`,
-      async function (err, result) {
-        // console.log(result);
-        if (result.length == 0) {
-          res.status(401).send("username/email invalid");
+  const { password, email, role } = req.headers;
+  if (!password || !email || !role) {
+    res.status(400).send({
+      status: "failed",
+      message: "Something not sent",
+    });
+    return;
+  }
+  con.query(`select * from ${role} where email="${email}"`, (err, result) => {
+    if (result.length == 0) {
+      res
+        .status(400)
+        .send({ status: "failed", error: "username/email invalid" });
+      return;
+    }
+    bcrypt.compare(
+      req.headers.password,
+      result[0].password,
+      (error, isMatch) => {
+        if (error) {
+          res.status(505).send({
+            status: "failed",
+            message: "username/email invalid",
+          });
           return;
         }
-        console.log(result[0]);
-        await bcrypt.compare(
-          req.headers.password,
-          result[0].password,
-          (error, isMatch) => {
-            console.log(isMatch);
-            if (isMatch) {
-              const accessToken = jwt.sign(
-                JSON.stringify({
-                  email: req.headers.email,
-                  role: req.headers.role,
-                  id:
-                    req.headers.role === "farmer"
-                      ? result[0].farmerID
-                      : result[0].contractorID,
-                }),
-                jwtSecret
-              );
-              console.log(
-                "lololo",
+
+        if (isMatch) {
+          const accessToken = jwt.sign(
+            JSON.stringify({
+              email: req.headers.email,
+              role: req.headers.role,
+              id:
                 req.headers.role === "farmer"
                   ? result[0].farmerID
-                  : result[0].contractorID
-              );
-              res.send({
-                accessToken,
-                id:
-                  req.headers.role === "farmer"
-                    ? result[0].farmerID
-                    : result[0].contractorID,
-              });
-              return;
-            } else {
-              console.log(error);
-              res.status(401).send("username/email invalid");
-              return;
-              // throw new Error("email or password is incorrect");
-            }
-          }
-        );
+                  : result[0].contractorID,
+            }),
+            jwtSecret
+          );
+          res.status(200).send({
+            accessToken,
+            id:
+              req.headers.role === "farmer"
+                ? result[0].farmerID
+                : result[0].contractorID,
+          });
+          return;
+        } else {
+          res
+            .status(401)
+            .send({ status: "failed", message: "username/email invalid" });
+          return;
+        }
       }
     );
-  };
-  a();
+  });
 });
 app.get("/details", (req, res) => {
   const { id } = req.headers;
-  console.log(id);
-  try {
-    con.query(`select * from demand where auto_id=${id};`, (error, result) => {
-      console.log(`select * from demand where auto_id=${id};`, result, error);
-      res.send(result);
+  if (!id) {
+    res.status(400).send({
+      status: "failed",
+      message: "data invalid",
     });
-  } catch {}
+    return;
+  }
+  try {
+    con.query(`select * from demand where auto_id=${id};`, (err, result) => {
+      if (err) {
+        res.status(505).send({
+          status: "failed",
+          message: "Data could not be fetched",
+        });
+        return;
+      }
+      res.status(200).send({
+        result,
+        status: "success",
+        message: "Data successfully fetched",
+      });
+    });
+  } catch (err) {}
 });
 app.get("/contractor/demand", (req, res) => {
-  console.log(req.headers, "helllllllllllllllllll");
   const { data } = req.headers;
-  console.log(data);
-  const x = JSON.parse(data);
-  console.log(x);
-  const id = x.id;
-  // res.send("id");
-  // if (!id) {
-  //   console.log(id);
-  //   res.send("no id passed");
-  //   return;
-  // }
+  const { id } = JSON.parse(data);
+  if (!id) {
+    res.status(400).send({
+      status: "failed",
+      message: "data invalid",
+    });
+    return;
+  }
   con.query(
     `select * from demand where contractorID=${id}`,
     (error, result) => {
-      if (error) console.log(error);
+      if (error) {
+        res.status(505).send({
+          status: "failed",
+          message: "data could  not be fetched",
+        });
+        return;
+      }
       res.status(200).send(result);
       return;
     }
   );
 });
 app.get("/signin/verify", (req, res) => {
-  console.log(req.headers);
   const { token } = req.headers;
-  // console.log(token, !token);
   if (!token) {
-    res.status(401).json({ error: "Invalid token" });
+    res.status(400).send({ error: "Invalid token" });
     return;
   }
 
   try {
     const decoded = jwt.verify(token, jwtSecret);
-    // console.log(decoded);
-    // console.log();
-    res.send(decoded);
+    res
+      .status(200)
+      .send({ jwt: decoded, status: "success", message: "Authenticated" });
   } catch (error) {
-    console.log(error);
-    res.status(401).json({ error: "Invalid token" });
+    res.status(400).json({ status: "failed", error: "Invalid token" });
   }
 });
 app.get("/update", (req, res) => {
-  console.log(req);
   const data = JSON.parse(req.headers.data);
-  console.log("hello");
   delete data["created_at"];
   console.log(
     `replace into demand(${Object.entries(data)
@@ -370,11 +503,18 @@ app.get("/update", (req, res) => {
       .map((entries) => JSON.stringify(entries[1]))
       .join(",")})`,
     (err, result) => {
-      console.log(err);
       if (err) {
-        res.status(500).send("yello");
-        console.log(err);
-      } else res.status(200).send("sab chenga si");
+        res.status(500).send({
+          status: "failed",
+          message: "Internal error",
+        });
+        return;
+      } else
+        res.status(200).send({
+          status: "success",
+          message: "data fetched",
+        });
+      return;
     }
   );
 });
