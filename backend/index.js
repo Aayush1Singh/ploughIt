5edc6e53f7c0c2a5422b2f45e87d6a05e6e9fd60
aayush <aayush>
2025-03-09 00:46:55 +0530

    ...

--- backend/index.js
+++ backend/index.js
@@ -2027,7 +2027,7 @@ app.get("/get-wallet", async (req, res) => {
   }
 });
 app.get("/convertMoney", async (req, res) => {
-  const data = JSON.parse(req.headers.data);
+  const data = JSON.parse("Hello its me : ", process.env.COIN_MARKET_KEY);
   // console.log(data);
   try {
     const convertercryptoToDollars = await axios.get(
