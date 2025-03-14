bb17fd6f9e0327a0aa78beeab627177664a15237
aayush <aayush>
2025-03-14 18:12:53 +0530

    added deletion workflow

--- ploughIt/src/database/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://givytcuxosjbfyhxhygj.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdpdnl0Y3V4b3NqYmZ5aHhoeWdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3OTEyMjgsImV4cCI6MjA1MTM2NzIyOH0.nVUhSgJDu_eO55VZzKZ7qSHkrVcoXpNarGMp5sOCL7Q";
-const supabase = createClient(supabaseUrl, supabaseKey);
-export default supabase;
